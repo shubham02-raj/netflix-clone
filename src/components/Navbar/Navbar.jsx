232f9d08989810bd5c5850b41aa197eb47c1818e
shubham <shubham.raj2015@gmail.com>
2025-04-03 23:01:39 +0530

    navbar added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import './Navbar.css';
 import logo from '../../assets/logo.png'
 import search_icon from '../../assets/search_icon.svg'
+import bell_icon from '../../assets/bell_icon.svg'
+import profile_img from '../../assets/profile_img.png'
+import caret_icon from '../../assets/caret_icon.svg'
 
 const Navbar = () => {
   return (
@@ -19,6 +22,15 @@ const Navbar = () => {
       </div>
       <div className="navbar-right">
         <img src={search_icon} alt="" className='icons' />
+        <p>Children</p>
+        <img src={bell_icon} alt="" className='icons' />
+        <div className="navbar-profile">
+        <img src={profile_img} alt="" className='profile' />
+        <img src={caret_icon} alt="" />
+        <div className="dropdown">
+          <p>Sign Out of Netflix</p>
+        </div>
+        </div>
       </div>
     </div>
   )
